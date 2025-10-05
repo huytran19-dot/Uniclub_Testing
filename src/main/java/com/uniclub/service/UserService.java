0c8838dcc913e58fa846ca6c7f3ac51978efbe0f
hualapthanh <hualapthanh@gmail.com>
2025-10-05 17:04:42 +0700

    Add UserService & UserServiceImpl

--- src/main/java/com/uniclub/service/UserService.java
+++ src/main/java/com/uniclub/service/UserService.java
@@ -1,4 +1,17 @@
 package com.uniclub.service;
 
+import com.uniclub.dto.request.User.CreateUserRequest;
+import com.uniclub.dto.request.User.UpdateUserRequest;
+import com.uniclub.dto.response.User.UserResponse;
+
+import java.util.List;
+
 public interface UserService {
+    List<UserResponse> getAllUsers();
+    UserResponse getUserById(Integer userId);
+
+    UserResponse createUser(CreateUserRequest request);
+    UserResponse updateUser(Integer userId, UpdateUserRequest request);
+    void deleteUser(Integer userId);
+    void inactiveUser(Integer userId);
 }
