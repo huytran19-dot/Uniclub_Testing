5cccbf8f1ce6c836fc7f2ce481387c20bf06af7f
hualapthanh <hualapthanh@gmail.com>
2025-10-05 17:04:15 +0700

    Add RoleService & RoleServiceImpl

--- src/main/java/com/uniclub/service/RoleService.java
+++ src/main/java/com/uniclub/service/RoleService.java
@@ -1,4 +1,17 @@
 package com.uniclub.service;
 
+import com.uniclub.dto.request.Role.CreateRoleRequest;
+import com.uniclub.dto.request.Role.UpdateRoleRequest;
+import com.uniclub.dto.response.Role.RoleResponse;
+
+import java.util.List;
+
 public interface RoleService {
+    List<RoleResponse> getAllRoles();
+    RoleResponse getRoleById(Integer id);
+
+    RoleResponse createRole(CreateRoleRequest request);
+    RoleResponse updateRole(Integer roleId, UpdateRoleRequest request);
+    void deleteRole(Integer id);
+    void inactiveRole(Integer userId);
 }
