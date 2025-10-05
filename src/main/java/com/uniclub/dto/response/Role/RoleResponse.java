9288e56828ae11c237592bf000d943914828ea77
hualapthanh <hualapthanh@gmail.com>
2025-10-05 17:03:08 +0700

    Add Role Response

--- src/main/java/com/uniclub/dto/response/Role/RoleResponse.java
+++ src/main/java/com/uniclub/dto/response/Role/RoleResponse.java
@@ -1,4 +1,28 @@
 package com.uniclub.dto.response.Role;
 
+import com.uniclub.entity.Role;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
 public class RoleResponse {
+    private Integer id;
+    private String name;
+    private String description;
+    private Byte status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public static RoleResponse fromEntity(Role role) {
+        RoleResponse response = new RoleResponse();
+        response.setId(role.getId());
+        response.setName(role.getName());
+        response.setDescription(role.getDescription());
+        response.setStatus(role.getStatus());
+        response.setCreatedAt(role.getCreatedAt());
+        response.setUpdatedAt(role.getUpdatedAt());
+
+        return response;
+    }
 }
