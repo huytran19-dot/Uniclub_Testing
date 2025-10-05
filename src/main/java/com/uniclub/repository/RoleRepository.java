fe025b9a20e4a8775790b2d050a630b27dd2ea87
hualapthanh <hualapthanh@gmail.com>
2025-10-05 17:03:40 +0700

    Add RoleRepository

--- src/main/java/com/uniclub/repository/RoleRepository.java
+++ src/main/java/com/uniclub/repository/RoleRepository.java
@@ -1,4 +1,14 @@
 package com.uniclub.repository;
 
-public interface RoleRepository {
+import com.uniclub.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByName(String name);
+
+    boolean existsByName(String name);
 }
