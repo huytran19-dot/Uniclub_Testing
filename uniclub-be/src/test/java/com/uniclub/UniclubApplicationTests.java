8717de12115af8b561e251d46283443b66df7b84
quangminh18112003 <146307541+quangminh18112003@users.noreply.github.com>
2025-11-25 22:29:38 +0700

    CI3

--- uniclub-be/src/test/java/com/uniclub/UniclubApplicationTests.java
+++ uniclub-be/src/test/java/com/uniclub/UniclubApplicationTests.java
@@ -3,7 +3,15 @@ package com.uniclub;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+	"spring.datasource.url=jdbc:h2:mem:uniclub;DB_CLOSE_DELAY=-1",
+	"spring.datasource.driver-class-name=org.h2.Driver",
+	"spring.datasource.username=sa",
+	"spring.datasource.password=",
+	"spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+	// use create to avoid noisy drop statements against an empty in-memory DB
+	"spring.jpa.hibernate.ddl-auto=create"
+})
 class UniclubApplicationTests {
 
 	@Test
